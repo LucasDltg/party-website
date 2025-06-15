cb2814e7291281d4ba3874904ad9ee04c19fd06d
LucasDltg <lucas.deletang.legravier@gmail.com>
2025-06-15 16:02:05 +0200

    fix: remove token on logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { auth } from '../lib/firebaseConfig'
 import { onAuthStateChanged, signOut, User } from 'firebase/auth'
-import nookies from 'nookies'
 
 export default function Header() {
   const [user, setUser] = useState<User | null>(null)
@@ -17,9 +16,10 @@ export default function Header() {
   }, [])
 
   const handleLogout = async () => {
+    await fetch('/api/logout', { method: 'POST' })
     await signOut(auth)
-    nookies.destroy(null, 'token') // clear token cookie on logout
   }
+  
 
   return (
     <header className="fixed top-0 left-0 right-0 bg-indigo-600 text-white flex justify-between items-center px-5 py-3 z-50">
