cb194be55b970043ce89158a7b749edf4d02e967
LucasDltg <lucas.deletang.legravier@gmail.com>
2025-07-19 18:48:17 +0200

    fix: async language fetch

--- src/app/[lang]/lang/page.tsx
+++ src/app/[lang]/lang/page.tsx
@@ -3,9 +3,10 @@ import { getDictionary } from '../../../lib/lang/dictionaries'
 export default async function Page({
   params,
 }: {
-  params: { lang: 'en' | 'fr' }
+  params: Promise<{ lang: 'en' | 'fr' }>
 }) {
-  const dict = await getDictionary(params.lang)
+  const p = await params
+  const dict = await getDictionary(p.lang)
 
   return <button>{dict.products.cart}</button>
 }
