41f983daef2da04e4cc20567d1ecfb7e3133a83e
LucasDltg <lucas.deletang.legravier@gmail.com>
2025-07-20 14:07:01 +0200

    fix: remove log

--- src/app/[lang]/[...slug]/page.tsx
+++ src/app/[lang]/[...slug]/page.tsx
@@ -1,15 +1,15 @@
 // src/app/[lang]/[...slug]/page.tsx
 import { notFound } from 'next/navigation'
 
-type PageProps = {
-  params: {
-    lang: string
-    slug?: string[]
-  }
-}
+// type PageProps = {
+//   params: {
+//     lang: string
+//     slug?: string[]
+//   }
+// }
 
-export default function CatchAllPage({ params }: PageProps) {
-  console.log('Caught params:', params)
+export default function CatchAllPage(/*{ params }: PageProps*/) {
+  // console.log('Caught params:', params)
   // params.lang = 'en'
   // params.slug = ['some', 'random', 'path'] or undefined
 
