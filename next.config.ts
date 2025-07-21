838cd499c4d2edd0181a766a78610a51fbf3aa8c
LucasDltg <lucas.deletang.legravier@gmail.com>
2025-07-21 19:04:26 +0200

    fix: turbopack warning

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from 'next'
-import path from 'path'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  webpack: (config) => {
-    // Fix alias resolution for "@/..." paths
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, 'src'),
-    }
-    return config
-  },
 }
 
 export default nextConfig
