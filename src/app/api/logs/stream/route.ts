184678f1aa2a3f8279433bbbafadee73e1aaf7b2
LucasX Ubuntu <lucas.deletang.legravier@gmail.com>
2025-11-14 14:20:48 +0100

    fix: stream logs

--- src/app/api/logs/stream/route.ts
+++ src/app/api/logs/stream/route.ts
@@ -3,29 +3,32 @@ import logger, { LogEntry } from '@/lib/logger'
 import { withAPIProtected } from '@/lib/firebase/withAPIProtected'
 
 // export async function GET(req: NextRequest) {
-export const GET = withAPIProtected(async (req) => {
-  const { readable, writable } = new TransformStream()
-  const writer = writable.getWriter()
+export const GET = withAPIProtected(
+  async (req) => {
+    const { readable, writable } = new TransformStream()
+    const writer = writable.getWriter()
 
-  writer.write(encode(`retry: 1000\n\n`))
+    writer.write(encode(`retry: 1000\n\n`))
 
-  const unsubscribe = logger.onLog((log) => {
-    writer.write(encodeLog(log))
-  })
+    const unsubscribe = logger.onLog((log) => {
+      writer.write(encodeLog(log))
+    })
 
-  req.signal.addEventListener('abort', () => {
-    unsubscribe()
-    writer.close()
-  })
+    req.signal.addEventListener('abort', () => {
+      unsubscribe()
+      writer.close()
+    })
 
-  return new NextResponse(readable, {
-    headers: {
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      Connection: 'keep-alive',
-    },
-  })
-})
+    return new NextResponse(readable, {
+      headers: {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        Connection: 'keep-alive',
+      },
+    })
+  },
+  { requiredRole: 'admin' },
+)
 
 function encodeLog(log: LogEntry) {
   return encode(`data: ${JSON.stringify(log)}\n\n`)
